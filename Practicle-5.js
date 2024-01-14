d6045ee405633eabf65c4f73671c6e3638c09e49
IT20636524 <IT20636524@my.sliit.lk>
2024-01-14 23:03:29 +0530

    static variables and methods

---
+++ Practicle-5.js
@@ -0,0 +1,12 @@
+// Static variables & static methods
+
+class MyClass {
+    static staticVariable = "Hello AF";
+
+    static staticMethod() {
+        console.log("This is a static method");
+    }
+}
+
+console.log(MyClass.staticVariable);
+MyClass.staticMethod();
